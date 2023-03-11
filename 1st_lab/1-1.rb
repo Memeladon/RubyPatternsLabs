5ba0c5690aeae65b53d5ea2993ffdee040da58f5
Memeladon <bingorussian@mail.ru>
2023-03-11 10:18:22 +0300

    Update 1-1.rb

--- 1st_lab/1-1.rb
+++ 1st_lab/1-1.rb
@@ -1,2 +1,19 @@
 puts("Hello world!")
-puts("Hello #{username}" )
+
+username = ARGV[0]
+ 
+  puts "Как тебя зовут?"
+  puts "Привет, #{username} \nКакой твой любимый язык программирования?"
+  answer = STDIN.gets.chop.downcase
+  case answer
+  when 'ruby'
+    puts "А может ты еще и лабы делаешь?"
+  when 'java'
+    puts "Годно, очень годно."
+  when 'python'
+    puts "Нормальный язык выучи для начала."
+  when 'c++'
+    puts "Ты мне нравишься. Продолжай в том же духе!"
+  else
+    puts 'Ну, время подумать еще есть...'
+  end
