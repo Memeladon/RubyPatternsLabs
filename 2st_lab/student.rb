f36d30099b6bcac9b8ea10635a82c2aa69371eb8
Memeladon <bingorussian@mail.ru>
2023-03-25 01:26:44 +0300

    Выполнил 4 пункт

--- 2st_lab/student.rb
+++ 2st_lab/student.rb
@@ -2,16 +2,16 @@
 class Student
 # Геттер и сеттер
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
-# Инициализация
-  def initialize(id:, last_name:, first_name:, middle_name: "", phone: "", telegram: "", email: "", github: "")
-    @id = id
-    @last_name = last_name
-    @first_name = first_name
-    @middle_name = middle_name
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @github = github
+# Инициализация с использованием атрибутов для упрощения написания
+ def initialize(id:, last_name:, first_name:, middle_name: "", phone: "", telegram: "", email: "", github: "")
+    self.id = id
+    self.last_name = last_name
+    self.first_name = first_name
+    self.middle_name = middle_name
+    self.phone = phone
+    self.telegram = telegram
+    self.email = email
+    self.github = github
   end
 # Правильность номера телефона
   def phone_valid?
