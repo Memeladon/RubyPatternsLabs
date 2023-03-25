0c0d51f0c672db699dde40918e2961297914895a
Memeladon <bingorussian@mail.ru>
2023-03-25 08:32:53 +0300

    5 задание - хэш

--- 2st_lab/student.rb
+++ 2st_lab/student.rb
@@ -2,17 +2,17 @@
 class Student
 # Геттер и сеттер
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
-# Инициализация с использованием атрибутов для упрощения написания
- def initialize(id:, last_name:, first_name:, middle_name: "", phone: "", telegram: "", email: "", github: "")
-    self.id = id
-    self.last_name = last_name
-    self.first_name = first_name
-    self.middle_name = middle_name
-    self.phone = phone
-    self.telegram = telegram
-    self.email = email
-    self.github = github
-  end
+# передаем в конструктор объект типа Hash и используем его для инициализации объекта
+def initialize(**options)
+  self.id = options[:id]
+  self.last_name = options[:last_name]
+  self.first_name = options[:first_name]
+  self.middle_name = options[:middle_name] || ""
+  self.phone = options[:phone] || ""
+  self.telegram = options[:telegram] || ""
+  self.email = options[:email] || ""
+  self.github = options[:github] || ""
+end
 # Правильность номера телефона
   def phone_valid?
     !!(/^(\+7|7|8)?[\d]{10}$/ =~ @phone)
