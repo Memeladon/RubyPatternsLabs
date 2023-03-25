a9b44b66e22653a8e9f101d5e74c78a6b9045ee1
Memeladon <bingorussian@mail.ru>
2023-03-25 08:49:40 +0300

    7 задание
    Добавление метода класса для проверки телефонного номера

--- 2st_lab/student.rb
+++ 2st_lab/student.rb
@@ -1,20 +1,27 @@
 # Создание класса
 class Student
-# Геттер и сеттер
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :github
-# передаем в конструктор объект типа Hash и используем его для инициализации объекта
-def initialize(**options)
-  self.id = options[:id]
-  self.last_name = options[:last_name]
-  self.first_name = options[:first_name]
-  self.middle_name = options[:middle_name] || ""
-  self.phone = options[:phone] || ""
-  self.telegram = options[:telegram] || ""
-  self.email = options[:email] || ""
-  self.github = options[:github] || ""
-end
-# Правильность номера телефона
+  # передаем в конструктор объект типа Hash и используем его для инициализации объекта
+  def initialize(**options)
+    self.id = options[:id]
+    self.last_name = options[:last_name]
+    self.first_name = options[:first_name]
+    self.middle_name = options[:middle_name] || ""
+    self.phone = options[:phone] || ""
+    self.telegram = options[:telegram] || ""
+    self.email = options[:email] || ""
+    self.github = options[:github] || ""
+  end
+
+  # Правильность номера телефона
   def phone_valid?
-    !!(/^(\+7|7|8)?[\d]{10}$/ =~ @phone)
+    !!(/^(\+7|7|8)?[\d]{10}$/ =~ phone)
+  end
+# Метод класса для проверки телефонного номера
+# Используем его для проверки номера при установке значения атрибута phone
+  def phone=(value)
+    raise ArgumentError, "Неправильный формат номера телефона" unless self.class.phone_valid?(value)
+    @phone = value
   end
 end
+
