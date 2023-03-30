799c81abcea53bcf3b821a5cd36e50793e5b8958
Memeladon <bingorussian@mail.ru>
2023-03-30 15:24:01 +0300

    2.1
    Доделал вывод под метод to_s

--- 2nd_lab/main.rb
+++ 2nd_lab/main.rb
@@ -4,22 +4,8 @@ student1 = Student.new(1, 'Позов', 'Дмитрий', 'Темурович', 
 student2 = Student.new(2, 'Шастун', 'Антон', 'Андреевич', '+79112234567', '@shastoon', 'shastoon@didenok.com', 'githubNO LINKS/shastoon')
 
 puts "Student 1:"
-puts "ID: #{student1.id}"
-puts "Last name: #{student1.last_name}"
-puts "First name: #{student1.first_name}"
-puts "Middle name: #{student1.middle_name}"
-puts "Phone number: #{student1.phone_number}"
-puts "Telegram: #{student1.telegram}"
-puts "Email: #{student1.email}"
-puts "GitHub: #{student1.github}"
+puts student1.to_s
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 puts "Student 2:"
-puts "ID: #{student2.id}"
-puts "Last name: #{student2.last_name}"
-puts "First name: #{student2.first_name}"
-puts "Middle name: #{student2.middle_name}"
-puts "Phone number: #{student2.phone_number}"
-puts "Telegram: #{student2.telegram}"
-puts "Email: #{student2.email}"
-puts "GitHub: #{student2.github}"
+puts student2.to_s
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
