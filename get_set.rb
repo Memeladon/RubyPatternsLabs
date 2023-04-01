e7c614a475c22208473788bf07062ee236003d3e
Memeladon <bingorussian@mail.ru>
2023-04-01 08:50:41 +0300

    Подсказка

---
+++ get_set.rb
@@ -0,0 +1,18 @@
+# attr_writer :age =
+def age=(value)
+  @age = value
+end
+
+# attr_reader :age =
+def age
+  @age
+end
+
+# attr_accessor :age =
+def age=(value)
+  @age = value
+end
+
+def age
+  @age
+end
