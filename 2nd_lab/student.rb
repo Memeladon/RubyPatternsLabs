4663463c92a5356018f73384cd22219ec45141cd
Memeladon <bingorussian@mail.ru>
2023-04-10 18:49:49 +0300

    8
    - Создать валидации для корректной формы строки в остальных полях класса Student.

--- 2nd_lab/student.rb
+++ 2nd_lab/student.rb
@@ -1,4 +1,3 @@
-
 # student.rb
 
 class Student
@@ -15,15 +14,49 @@ class Student
     @github = contacts[:github]
   
     validate_phone_number
-end
+    validate_last_name
+    validate_first_name
+    validate_middle_name
+    validate_telegram
+    validate_email
+    validate_github
+  end
 
-def validate_phone_number
-  # Обработка исключений // Оператор unless проверяет отрицание
-  raise "Неправильно набран номер" unless @phone_number =~ /^\+\d{11}$/
-end
+  def validate_phone_number
+    raise "Неправильный формат номера" unless @phone_number =~ /^\+\d{11}$/
+  end
+
+  def validate_last_name
+    raise "Неправильный формат Фамилии" unless @last_name =~ /^[A-Za-z\-]+$/
+  end
+
+  def validate_first_name
+    raise "Неправильный формат имени" unless @first_name =~ /^[A-Za-z\-]+$/
+  end
+
+  def validate_middle_name
+    raise "Неправильный формат Отчества" unless @middle_name =~ /^[A-Za-z\-]*$/
+  end
+
+  def validate_telegram
+    raise "Неправильный формат телеграмма" unless @telegram =~ /^@[A-Za-z]*$/
+  end
+
+  def validate_email
+    raise "Неправильный формат почты" unless @email =~ /^([A-Za-z0-9]+[_\.\-]?)*[A-Za-z0-9]+@([A-Za-z0-9]+[\.\-]?)*[A-Za-z0-9]+\.[A-Za-z]{2,}$/
+  end
+
+  def validate_github
+    raise "Неправильный формат гита" unless @github =~ /^https:\/\/githubNO LINKS\/[A-Za-z0-9_\-]+\/?$/
+  end
 
   def to_s
-    "ID: #{@id}\nLast name: #{@last_name}\nFirst name: #{@first_name}\nMiddle name: #{@middle_name}\nPhone number: #{@phone_number}\nTelegram: #{@telegram}\nEmail: #{@email}\nGitHub: #{@github}"
+    "ID: #{@id}\nФамилия: #{@last_name}\nИмя: #{@first_name}\nОтчество: #{@middle_name}\nНомер: #{@phone_number}\nTelegram: #{@telegram}\nEmail: #{@email}\nGitHub: #{@github}"
   end
 
 end
+
+
+
+
+
