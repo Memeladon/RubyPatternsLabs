3aac9932d0afbf06302969e48b6253e1c915c4d4
Memeladon <bingorussian@mail.ru>
2023-04-10 18:26:09 +0300

    7
    - Добавит метод класса, проверяющий, является ли некоторая строка телефонным номером.
    - Модифицировал класс Student, чтобы нельзя было создать объект с неправильным номером телефона.
    - Протестировал класс Student.
    - Редактировал main.rb чтобы проверить работоспособность. Ошибка выдается при неправильном вводе номера.

--- 2nd_lab/student.rb
+++ 2nd_lab/student.rb
@@ -1,4 +1,6 @@
 
+# student.rb
+
 class Student
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone_number, :telegram, :email, :github
 
@@ -11,9 +13,17 @@ class Student
     @telegram = contacts[:telegram]
     @email = contacts[:email]
     @github = contacts[:github]
-  end
+  
+    validate_phone_number
+end
+
+def validate_phone_number
+  # Обработка исключений // Оператор unless проверяет отрицание
+  raise "Неправильно набран номер" unless @phone_number =~ /^\+\d{11}$/
+end
 
   def to_s
     "ID: #{@id}\nLast name: #{@last_name}\nFirst name: #{@first_name}\nMiddle name: #{@middle_name}\nPhone number: #{@phone_number}\nTelegram: #{@telegram}\nEmail: #{@email}\nGitHub: #{@github}"
-    end
+  end
+
 end
