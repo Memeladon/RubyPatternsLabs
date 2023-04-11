6bca96d2ea3ddcf005279480c2bb0220b6fb6716
Memeladon <bingorussian@mail.ru>
2023-04-11 03:09:09 +0300

    1
    - Написал отдельный конструктор, принимающий на вход строку, который эту строку парсит.
    - Вызывается он в стандартном конструкторе с валидациями

--- 2nd_lab/Part_2/student.rb
+++ 2nd_lab/Part_2/student.rb
@@ -3,18 +3,25 @@
 class Student
   attr_accessor :id, :last_name, :first_name, :middle_name, :phone_number, :telegram, :email, :github
 
-  def initialize(id:, last_name:, first_name:, middle_name:, **contacts)
-    @id = id
-    @last_name = last_name
-    @first_name = first_name
-    @middle_name = middle_name
-    @phone_number = contacts[:phone_number]
-    @telegram = contacts[:telegram]
-    @email = contacts[:email]
-    @github = contacts[:github]
-  
+  def initialize(data)
+    parse_data(data)
+    
     validate_phone_number_presence
     validate_contact_presence
+  end
+
+  def parse_data(data)
+    parts = data.split(';')
+    raise ArgumentError, "Неправильный формат данных" unless parts.size == 8
+
+    @id = parts[0].to_i
+    @last_name = parts[1]
+    @first_name = parts[2]
+    @middle_name = parts[3]
+    @phone_number = parts[4]
+    @telegram = parts[5]
+    @email = parts[6]
+    @github = parts[7]
   end
 
   def set_contacts(**contacts)
