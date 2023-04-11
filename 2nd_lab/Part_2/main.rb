6bcdd8ee5a5b971f5139d3eaa5a77a2e33754b69
Memeladon <bingorussian@mail.ru>
2023-04-11 03:15:16 +0300

    1
    - Проверка ввода в main.rb

--- 2nd_lab/Part_2/main.rb
+++ 2nd_lab/Part_2/main.rb
@@ -1,17 +1,13 @@
 require_relative 'student.rb'
 
-student1 = Student.new(id:1, last_name:'Позов', first_name:'Дмитрий', middle_name:'Темурович', phone_number:'+79111234567', github:'githubNO LINKS/pozov')
-student2 = Student.new(id:2, last_name:'Шастун', first_name:'Антон', middle_name:'Андреевич', phone_number:'+79112234567',
-    telegram:'@shastoon', email:'shastoon@didenok.com', github:'githubNO LINKS/shastoon')
-student2.set_contacts(phone_number: "+79111111111", telegram: "@shastoon_new", email: "shastoon@mail.com")
+student1 = Student.new('1;Позов;Дмитрий;Темурович;+79111234567;;;githubNO LINKS/pozov')
+student2 = Student.new('2;Шастун;Антон;Андреевич;+79112234567;@shastoon;shastoon@didenok.com;githubNO LINKS/shastoon')
+# student2.set_contacts(phone_number: "+79111111111", telegram: "@shastoon_new", email: "shastoon@mail.com")
 
 puts "Student 1:"
 puts student1.to_s
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 puts "Student 2:"
-puts student2.to_s
-puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-puts "Student 2 new:"
 puts student2.to_s
 puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
