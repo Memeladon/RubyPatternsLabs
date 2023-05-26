10307f06f508404331986eb5a2d3e36d989901b4
Memeladon <bingorussian@mail.ru>
2023-05-27 02:24:22 +0300

    1.
    - Инициализация

--- 2nd_lab/main.rb
+++ 2nd_lab/main.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'student.rb'
 
 student1 = Student.new(id:1, last_name:'Ppzov', first_name:'Dmitry', surname:'Temurovich', phone_number:'8-800-555-35-35')
--- 2nd_lab/student.rb
+++ 2nd_lab/student.rb
@@ -1,2 +1,16 @@
-# frozen_string_literal: true
+class Student
+  attr_accessor :id, :surname, :name, :patronymic, :phone, :telegram, :email, :git
 
+  def initialize(args = {})
+    @id = args[:id]
+    @surname = args[:surname]
+    @name = args[:name]
+    @patronymic = args[:patronymic]
+    @phone = args[:phone]
+    @telegram = args[:telegram]
+    @email = args[:email]
+    @git = args[:git]
+  end
+
+
+end
