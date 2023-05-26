37ee23228b273802b00dcc840bf0c55c165e6e70
Memeladon <bingorussian@mail.ru>
2023-05-27 02:40:25 +0300

    4
    - Необязательные поля

--- 2nd_lab/main.rb
+++ 2nd_lab/main.rb
@@ -1,10 +1,10 @@
 require_relative 'student.rb'
 
-student1 = Student.new(id:1, last_name:'Ppzov', first_name:'Dmitry', surname:'Temurovich', phone_number:'8-800-555-35-35')
-student2 = Student.new(id:2, last_name:'Shastoon', first_name:'Anton', surname:'Andreevich', phone_number:'8-800-555-35-35',
-                       telegram:'@shastoon', email:'shastoon@didenok.com', github:'githubNO LINKS/shastoon')
-student2.set_contacts(phone_number: "8-800-000-00-01", telegram: "@shastoon", email: "shastoon@mail.com",
-                      github:'githubNO LINKS/pozov')
+student1 = Student.new(id:1, surname:'Pozov', name:'Dmitry', patronymic:'Temurovich', phone:'8-800-555-35-35')
+student2 = Student.new(id:2, surname:'Shastoon', name:'Anton', patronymic:'Andreevich', phone:'8-800-555-35-35',
+                       telegram:'@shastoon', email:'shastoon@didenok.com', git:'githubNO LINKS/shastoon')
+# student2.set_contacts(phone: "8-800-000-00-01", telegram: "@shastoon", email: "shastoon@mail.com",
+#                       git:'githubNO LINKS/pozov')
 
 
 puts "Student 1:"
--- 2nd_lab/student.rb
+++ 2nd_lab/student.rb
@@ -3,14 +3,17 @@ class Student
 
   def initialize(args = {})
     @id = args[:id]
-    @surname = args[:surname]
-    @name = args[:name]
-    @patronymic = args[:patronymic]
-    @phone = args[:phone]
-    @telegram = args[:telegram]
-    @email = args[:email]
-    @git = args[:git]
+    @surname = args[:surname] || ''
+    @name = args[:name] || ''
+    @patronymic = args[:patronymic] || ''
+    @phone = args[:phone] || ''
+    @telegram = args[:telegram] || ''
+    @email = args[:email] || ''
+    @git = args[:git] || ''
   end
 
+  def to_s
+    "ID: #{@id}\nSurname: #{@surname}\nName: #{@name}\nPatronymic: #{@patronymic}\nPhone number: #{[@phone]}\nTelegram: #{@telegram}\nEmail: #{@email}\nGitHub: #{@git}"
+  end
 
 end
