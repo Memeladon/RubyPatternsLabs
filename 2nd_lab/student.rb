af79d0b88ad73bcfdd18ea283076764dc8355044
Memeladon <bingorussian@mail.ru>
2023-05-27 02:47:26 +0300

    7
    - Валидация номера телефона

--- 2nd_lab/student.rb
+++ 2nd_lab/student.rb
@@ -7,13 +7,22 @@ class Student
     @name = args[:name] || ''
     @patronymic = args[:patronymic] || ''
     @phone = args[:phone] || ''
+
+    raise ArgumentError, 'Invalid phone number' unless Student.valid_phone_number?(@phone)
+
     @telegram = args[:telegram] || ''
     @email = args[:email] || ''
     @git = args[:git] || ''
   end
 
+  def self.valid_phone_number?(phone)
+    phone.to_s.match?(/^(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d$/)
+  end
+
   def to_s
     "ID: #{@id}\nSurname: #{@surname}\nName: #{@name}\nPatronymic: #{@patronymic}\nPhone number: #{[@phone]}\nTelegram: #{@telegram}\nEmail: #{@email}\nGitHub: #{@git}"
   end
+
+
 
 end
