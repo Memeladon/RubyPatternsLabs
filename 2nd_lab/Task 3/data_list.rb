02cc3077ae5db5c97cb338b18020060f78820df3
Memeladon <bingorussian@mail.ru>
2023-05-29 02:53:51 +0300

    9

--- 2nd_lab/Task 3/data_list.rb
+++ 2nd_lab/Task 3/data_list.rb
@@ -1,8 +1,13 @@
 class Data_list
-  attr_reader :data
+  attr_reader :data, :names
   def initialize(data)
-    @data = data
+    @names = get_names
+    self.data = data
     @selected = []
+  end
+
+  def data=(data)
+    @data = validated_data(data)
   end
   def select(number)
     @selected << number unless @selected.include?(number)
@@ -16,6 +21,16 @@ class Data_list
   end
   def get_data(obj)
     raise NotImplementedError, "This method is implemented in subclasses"
+  end
+
+  private
+  def validated_data(data)
+    raise ArgumentError, "Data should be an array" unless data.is_a?(Array)
+    raise ArgumentError, "Data should not be empty" if data.empty?
+    raise ArgumentError, "Data elements should have the same structure" unless data.all? { |obj| obj.instance_of?(data[0].class) }
+    raise ArgumentError, "Data elements should have properties #{names}" unless data.all? { |obj| names.all? { |prop| obj.respond_to?(prop) } }
+
+    data
   end
 
 end
--- 2nd_lab/Task 3/data_table.rb
+++ 2nd_lab/Task 3/data_table.rb
@@ -3,6 +3,13 @@ class Data_table
   def initialize(data)
     @data = data
   end
+
+  def set_data(new_data)
+    @data = new_data.map.with_index do |row, i|
+      row.map.with_index { |element, j| element.nil? ? get_element(i, j) : element }
+    end
+  end
+
   def get_element(row, col)
     @data[row][col]
   end
